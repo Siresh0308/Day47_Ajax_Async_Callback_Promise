3b503225e957bb072e410c007328096c889a8b2e
Resh@308 <reshsingh1995@gmail.com>
2022-09-19 22:10:32 +0530

    DAy 47 Ajqax Problem Added

---
+++ Rest-html/HttpServices.js
@@ -0,0 +1,38 @@
+function makePromiseCall(methodType, url, async = true , data = null) {
+    return new Promise(function (resolve, reject){
+        let xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            // console.log("State changed called . Ready State :"+xhr.readyState+" Status :"+xhr.status);
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200 || xhr.status === 201) {
+                    resolve(xhr.responseText);
+                } else if (xhr.status >= 400) {
+                    reject({
+                        status:xhr.status,
+                        statusText:xhr.statusText
+                    })
+                    console.log("XHR Failed")
+                }
+            }
+        }
+        xhr.onerror=function (){
+            reject({
+                status:this.status,
+                statusText:this.statusText
+            })
+        }
+        xhr.open(methodType, url, async);
+        console.log(methodType + " request sent to the server")
+        if (data) {
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(data));
+        } else{
+            xhr.send();
+            console.log(methodType+ " Request sent to the server");
+
+
+        
+        }
+    });
+}
